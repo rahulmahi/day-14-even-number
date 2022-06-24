4a84bb0b01a8e81165d2795e4b68f10c209ae2a6
rahul <rahul.valasai@gmail.com>
2022-06-24 10:43:16 +0530

    even-number-or-not.

---
+++ even.js
@@ -0,0 +1,12 @@
+let n = prompt("enter the number");
+
+for(i = 1;i <=n ;i++)
+{
+    if(i % 2 == 0)
+    {
+        console.log("even number is",i)
+    }
+    else{
+        console.log("not even number",i);
+    }
+}
